54cf1f7b7db405371084d35e28743f0a7d957762
Bhavesh-Mankar51 <bhaveshmankar770@gmail.com>
2025-04-19 18:08:24 +0530

    remove decouple

--- src/coinlaunch/settings.py
+++ src/coinlaunch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-+wrke!lt^g2a4m@w3nz57lz4msoam@@sdizimi4xcji4(6!46%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
